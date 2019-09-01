5e52cfdde1cc3b6d05820e3b4703ca22e671f7ce
cayode@live.com <cayode@live.com>
2019-09-01 07:07:09 +0100

    The side bar is chopped at the bottom. I'm deliberating on fixing the top.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,59 @@ function App() {
 
       </section>
       <section className = "container">
+        <h1>All my Seeking Alpha articles</h1>
+        <p>
+          I guys, here are the links to all my published works on Seeking Alpha. It's been fun for me. I hope you enjoy them.
+        </p>
         <ul>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
+          <li>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
+          </li>
           <li>
           <a href="https://www.w3schools.com">Visit W3Schools</a>
           </li>
