78e2dfdfe71e8d89c10fdaca4ab301601a7da384
cayode@live.com <cayode@live.com>
2019-09-07 17:36:27 +0100

    Updates

---
+++ precache-manifest.a932739a320aeb2904d9135573cdca37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432fef9c434c4b36421f765722e352cc",
+    "url": "/sa-articles/index.html"
+  },
+  {
+    "revision": "e0de1022b20fa2637923",
+    "url": "/sa-articles/static/css/main.36496d89.chunk.css"
+  },
+  {
+    "revision": "0be629356da1de3eaf20",
+    "url": "/sa-articles/static/js/2.2a5d269c.chunk.js"
+  },
+  {
+    "revision": "e0de1022b20fa2637923",
+    "url": "/sa-articles/static/js/main.4494eb17.chunk.js"
+  },
+  {
+    "revision": "e1ecc60d7e89636530fb",
+    "url": "/sa-articles/static/js/runtime~main.faed0264.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sa-articles/static/media/logo.5d5d9eef.svg"
+  }
+]);
